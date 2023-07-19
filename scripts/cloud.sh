ca1e65928f459d48a6ec35717a0adc9cc2e51a49
Alex Lanz <alex.lanz@aboutbits.it>
2023-07-19 07:43:02 +0200

    install Terraform

--- scripts/cloud.sh
+++ scripts/cloud.sh
@@ -13,6 +13,9 @@ main() {
     brew_install 'Kubernetes CLI' 'kubectl'
     brew_install 'Kubernetes Helm' 'helm'
 
+    brew_tap 'Terraform' 'hashicorp/tap'
+    brew_install 'Terraform' 'hashicorp/tap/terraform'
+
     brew_install 'DigitalOcean CLI' 'doctl'
 
     brew_install 'AWS CLI' 'awscli'
--- utils/brew.sh
+++ utils/brew.sh
@@ -26,3 +26,25 @@ brew_install() {
     fi
 
 }
+
+brew_tap() {
+
+    declare -r TAP_DESCRIPTION="$1"
+    declare -r TAP_NAME="$2"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Check if `Homebrew` is installed
+
+    if ! cmd_exists 'brew'; then
+        print_error "$APPLICATION_DESCRIPTION (\`brew\` is not installed)"
+        return 1
+    fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Reference the specified tap
+
+    execute "brew tap $TAP_NAME" "$TAP_DESCRIPTION"
+
+}
